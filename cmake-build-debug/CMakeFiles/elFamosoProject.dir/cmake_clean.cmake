2afee9db646ac043538033507163d77958d92863
Cpasmoi91560 <corentin.delage@hotmail.fr>
2021-01-06 19:11:54 +0100

    first commit

---
+++ cmake-build-debug/CMakeFiles/elFamosoProject.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/elFamosoProject.dir/main.cpp.o"
+  "elFamosoProject"
+  "elFamosoProject.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/elFamosoProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/elFamosoProject.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for elFamosoProject.
+# This may be replaced when dependencies are built.
