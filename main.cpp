a1e4c74af043da3ac6c57a469e5f564a275473d8
Corentin91560 <corentin.delage@hotmail.fr>
2021-01-18 19:39:31 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <list>
 #include "Class/Rectangle.h"
 #include "Class/Dessin.h"
+#include "Class/Cercle.h"
 
 
 void affichemenu(){
@@ -33,37 +34,91 @@ void addformemenu(){
     std::cout << "Polygone 4" << std::endl;
 
 }
+Rectangle createRectangle(){
+    Rectangle rectangle;
+    int height;
+    int width;
+    int posx;
+    int posy;
+    std::string fill;
+    std::cout << "MENU CREATION RECTANGLE : \n" << std::endl;
+    std::cout << "Hauteur :" << std::endl;
+    std::cin >> height;
+    std::cout << "Largeur :" << std::endl;
+    std::cin >> width ;
+    std::cout << "posx :" << std::endl;
+    std::cin >> posx;
+    std::cout << "posy :" << std::endl;
+    std::cin >> posy ;
+    std::cout << "fill :" << std::endl;
+    std::cin >> fill;
+    rectangle.setHeight(height);
+    rectangle.setWidth(width);
+    rectangle.setPosx(posx);
+    rectangle.setPosy(posy);
+    rectangle.setFill(fill);
+    return rectangle;
+}
+Cercle createCercle(){
+    Cercle cercle;
+    int radius;
+    int posx;
+    int posy;
+    std::string fill;
+    std::cout << "MENU CREATION RECTANGLE : \n" << std::endl;
+    std::cout << "Radius :" << std::endl;
+    std::cin >> radius;
+    std::cout << "posx :" << std::endl;
+    std::cin >> posx;
+    std::cout << "posy :" << std::endl;
+    std::cin >> posy ;
+    std::cout << "fill :" << std::endl;
+    std::cin >> fill;
+    cercle.setRadius(radius);
+    cercle.setPosx(posx);
+    cercle.setPosy(posy);
+    cercle.setFill(fill);
+    return cercle;
+}
+void describeDessin(Dessin &dessin){
+    std::cout<<"Hauteur du dessin : "<<dessin.getHeight()<<"et Largueur : "<<dessin.getWidth()<<std::endl;
+    printf("%lu",dessin.formes.size());
+    for (int i = 0; i < dessin.formes.size(); ++i) {
+        std::cout<<dessin.formes[i]->getcontent()<<std::endl;
+    }
+}
 
-void selectforme(){
-    Rectangle rectangle = Rectangle(10,40,20,15,"pink");
-    std::vector<Forme> formes;
-    formes.push_back(rectangle);
-    Dessin dessin = Dessin(150,200,formes);
-    std::vector<Forme> forme = dessin.formes;
-    Rectangle rect1 = forme[0]; //rectangle
+Dessin selectforme(Dessin dessin){
+
+    printf("%lu",dessin.formes.size());
 
     int selection = 0;
-    bool selectable = false;
-    while (!selectable){
+    while (true){
         addformemenu();
         std::cin >> selection;
         switch (selection) {
             case 1:
-                printf("%f",rectangle.getPosx());
-                selectable = true;
+                dessin.formes.push_back(new Rectangle(createRectangle()));
                 break;
             case 2:
-                selectable = true;
+                dessin.formes.push_back(new Cercle(createCercle()));
 //cercle
                 break;
             case 3:
-                selectable = true;
+
 //segment
                 break;
             case 4:
-                selectable = true;
+
 //polygone
                 break;
+            case 5:
+                describeDessin(dessin);
+                break;
+            case 9:
+                printf("%lu",dessin.formes.size());
+                return dessin;
+//polygone
             default:
                 std::cout << "Entrée incorrecte, veuillez choisir une valeur appropriée\n" << std::endl;
                 break;
@@ -71,7 +126,9 @@ void selectforme(){
     }
 }
 
-void editionmenu(){
+void editionmenu(Dessin &dessin){
+    printf("test %lu debut",dessin.formes.size());
+
     int selection = 0;
     bool selectable = false;
     while (!selectable){
@@ -79,8 +136,8 @@ void editionmenu(){
         std::cin >> selection;
         switch (selection) {
             case 1:
-                selectable = true;
-                selectforme();
+                printf("test %lu end1",dessin.formes.size());
+                dessin = Dessin(selectforme(dessin));
                 break;
             case 2:
                 selectable = true;
@@ -101,19 +158,25 @@ void editionmenu(){
     }
 }
 
-void affichermenucreation(int* dimension){
+Dessin affichermenucreation(Dessin &dessin){
+    int height;
+    int width;
     std::cout << "MENU CREATION DESSIN : \n" << std::endl;
     std::cout << "Hauteur :" << std::endl;
-    std::cin >> dimension[0];
+    std::cin >> height;
     std::cout << "Largeur :" << std::endl;
-    std::cin >> dimension[1] ;
-    std::cout << "Hauteur : " << dimension[0] << "Largeur : " << dimension[1] << std::endl;
+    std::cin >> width ;
+    dessin.setHeight(height);
+    dessin.setWidth(width);
+    std::cout << "Hauteur : " << height << "Largeur : " << width << std::endl;
+    return dessin;
 }
 
 void creationdessin(){
-    int dimension[2];
-    affichermenucreation(dimension);
-    editionmenu();
+    std::vector<Forme*> formes;
+    Dessin dessin = Dessin(formes);
+    affichermenucreation(dessin);
+    editionmenu(dessin);
 }
 
 void startprogram(){
